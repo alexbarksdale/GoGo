adc129dea995574305d0c50e853e5355e7af6abe
alexbarksdale <ale.barksdale@gmail.com>
2020-03-07 23:02:03 -0600

    [Lesson] Type and converting types

---
+++ chapter4/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+var y = 42
+var a int
+
+type hotdog int
+
+var b hotdog
+
+func main() {
+	fmt.Println(y)
+	fmt.Printf("%T\n", y)
+
+	fmt.Printf("Variable declared with type\n")
+
+	a = 55
+	fmt.Println(a)
+	fmt.Printf("%T\n", a)
+
+	fmt.Printf("Custom type\n")
+
+	b = 43
+	fmt.Println(b)
+	fmt.Printf("%T\n", b)
+
+	fmt.Printf("Converting a type\n")
+	a = int(b)
+	fmt.Println(a)
+	fmt.Printf("%T\n", a)
+
+}
