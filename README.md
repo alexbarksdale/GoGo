56bb6d9d6e2a5fdc71e8c753b8ac484e6e6bd5a9
alexbarksdale <ale.barksdale@gmail.com>
2020-05-14 21:23:56 -0500

    Poitners

--- README.md
+++ README.md
@@ -10,3 +10,22 @@
 
 ### Why don't we have to import a function from other files?
 - Because it's under the same package
+
+# Pointers
+
+- &variable - Gives a memory address of the value this variable is pointing to
+- *pointer - Gives the value this memory address is pointing at
+
+### Example and explanation
+Assume `person` is a struct
+```go
+func (pointerToPerson *person) updateName() {
+    *pointerToPerson
+}
+```
+*person - This is a type description it means we're working with a poitner to a person
+*pointerToPerson - This is an operator, it means we want to manipulate the value the pointer is referencing
+
+### Conversion
+- Turn `address` into `value` with `*address`
+- Turn `value` into `address` with `&value`
---
+++ structs/main.go
@@ -0,0 +1,58 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+func main() {
+	// WAYS OF INIT A STRUCT TYPE
+	// ===================================
+
+	// 1. This relies heavily on the order of the struct.
+	// rob := person{"Rob", "Ery"}
+
+	// 2. Defines the fields of the struct
+	// rob := person{firstName: "Rob", lastName: "Ery"}
+	// fmt.Println(alex)
+
+	// 3. Go assigns a 0 value when creating var of type struct like this
+	//    0 value examples: strings = "" | int = 0 | bool = false
+	// 	  Ex: firstName = "" && lastName = ""
+	// var rob person
+	// fmt.Println(rob)
+	// fmt.Printf("%+v", rob) // %+v = print all the fields and their values
+
+	// rob.firstName = "Rob"
+	// rob.lastName = "Ery"
+	// fmt.Printf("\n%+v", rob)
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Emy",
+		contactInfo: contactInfo{
+			email:   "jim@test.com",
+			zipCode: 76321,
+		},
+	}
+
+	jimPointer := &jim
+	jimPointer.updateName("jimmy")
+	jim.info()
+}
+
+func (p person) info() {
+	fmt.Printf("\n%+v", p)
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
