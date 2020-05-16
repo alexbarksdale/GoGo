ef7fd44b5c5e110c70411ff65b26bf02c09e9ac1
alexbarksdale <ale.barksdale@gmail.com>
2020-05-15 23:48:35 -0500

    JWT

--- echo/src/main/main.go
+++ echo/src/main/main.go
@@ -6,7 +6,10 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
@@ -27,6 +30,11 @@ type Hamster struct {
 	Type string `json:"type"`
 }
 
+type JwtClaims struct {
+	Name string `json:"name"`
+	jwt.StandardClaims
+}
+
 func main() {
 	// New echo instance
 	e := echo.New()
@@ -40,15 +48,17 @@ func main() {
 	e.POST("/dogs", addDog)
 	e.POST("/hamsters", addHamster)
 
-	// GROUPING
-	g := e.Group("/admin")
+	e.GET("/login", login)
+
+	// ADMIN GROUPING
+	adminGrp := e.Group("/admin")
 
 	// logs the server interaction
-	g.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+	adminGrp.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: `[${time_rfc3339}] ${status} ${method} ${host}${path} ${latency_human}` + "\n",
 	}))
 
-	g.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+	adminGrp.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
 		// *Check in the DB if the password valid*
 		if username == "bob" && password == "123" {
 			return true, nil
@@ -56,7 +66,22 @@ func main() {
 		return false, nil
 	}))
 
-	g.GET("/main", mainAdmin)
+	adminGrp.GET("/main", mainAdmin)
+
+	// COOKIE GROUPING
+	cookieGrp := e.Group("/cookie")
+	cookieGrp.Use(checkCookie)
+
+	cookieGrp.GET("/main", mainCookie)
+
+	// JWT GROUPING
+	jwtGrp := e.Group("/jwt")
+	jwtGrp.Use(middleware.JWTWithConfig(middleware.JWTConfig{
+		SigningMethod: "HS512",
+		SigningKey:    []byte("dontUseThis"),
+	}))
+
+	jwtGrp.GET("/main", mainJwt)
 
 	// Starts echo instance
 	e.Start(":8000")
@@ -143,8 +168,71 @@ func addHamster(c echo.Context) error {
 	return c.String(http.StatusOK, "We got your hamster")
 }
 
+func login(c echo.Context) error {
+	username := c.QueryParam("username")
+	password := c.QueryParam("password")
+
+	// *check user and pass in db*
+	if username == "bob" && password == "123" {
+		cookie := &http.Cookie{}
+
+		// Same thing
+		// cookie := new(http.Cookie)
+
+		cookie.Name = "sessionID"
+		cookie.Value = "example_string"
+		cookie.Expires = time.Now().Add(48 * time.Hour)
+
+		c.SetCookie(cookie)
+
+		token, err := createJwtToken()
+		if err != nil {
+			log.Println("Error creating JWT token", err)
+			return c.String(http.StatusInternalServerError, "Something went wrong")
+		}
+
+		return c.JSON(http.StatusOK, map[string]string{
+			"message": "You were logged in",
+			"token":   token,
+		})
+	}
+	return c.String(http.StatusUnauthorized, "You were not logged in")
+}
+
+func createJwtToken() (string, error) {
+	claims := JwtClaims{
+		"bob",
+		jwt.StandardClaims{
+			Id:        "main_user_id",
+			ExpiresAt: time.Now().Add(24 * time.Hour).Unix(),
+		},
+	}
+
+	rawToken := jwt.NewWithClaims(jwt.SigningMethodHS512, claims)
+	token, err := rawToken.SignedString([]byte("dontUseThis"))
+	if err != nil {
+		return "", err
+	}
+	return token, nil
+}
+
 func mainAdmin(c echo.Context) error {
 	return c.String(http.StatusOK, "Secret admin page")
+}
+
+func mainCookie(c echo.Context) error {
+	return c.String(http.StatusOK, "You are viewing this because you have a cookie")
+}
+
+func mainJwt(c echo.Context) error {
+	user := c.Get("user")
+	token := user.(*jwt.Token)
+
+	claims := token.Claims.(jwt.MapClaims)
+
+	log.Println("Username:", claims["name"], "UserID:", claims["jti"])
+
+	return c.String(http.StatusOK, "you are on the super secret page!")
 }
 
 // ======= MIDDLEWARE ======
@@ -155,5 +243,24 @@ func ServerHeader(next echo.HandlerFunc) echo.HandlerFunc {
 		c.Response().Header().Set(echo.HeaderServer, "TestServer/1.0")
 		c.Response().Header().Set("Anything", "CrazyHeader")
 		return next(c)
+	}
+}
+
+func checkCookie(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		cookie, err := c.Cookie("sessionID")
+		if err != nil {
+			fmt.Println(err)
+			if strings.Contains(err.Error(), "named cookie not present") {
+				return c.String(http.StatusUnauthorized, "you don't have any cookie")
+			}
+			return err
+		}
+
+		if cookie.Value == "example_string" {
+			return next(c)
+		}
+
+		return c.String(http.StatusUnauthorized, "You don't have the right cookie")
 	}
 }
